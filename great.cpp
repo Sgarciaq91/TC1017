4e71ddb8bd4afc0e6a03d586108f1600ead213ed
Sgarciaq91 <sgarciaq91@gmail.com>
2015-04-06 09:24:23 -0500

    WSQ12

--- great.cpp
+++ great.cpp
@@ -2,33 +2,32 @@
 #include <cmath>
 using namespace std;
 
-int gcd(int a,int b){
+int GCD(int a, int b){
   if(a==b){
-  return a;
-}
-
-  if (a>b){
-  return gcd(a-b,b);
-}
-
+    return a;
+  }
+  if(a > b){
+    return GCD(a-b,b);
+  }
   if(b>a){
-  return gcd(a,b-a);
-}
+    return GCD(a, b-a);
+  }
+
 }
 
-int main (){
-
+int main ()
+{
 int first, second;
 
-cout<<"This program gives you the greatest common divisor of two integers given"<<endl;
-cout<<"Give me your first parameter"<<endl;
+cout<<"This program gives you greatest common divisor"<<endl;
+cout<<"Give me the first number"<<endl;
 cin>>first;
-cout<<"Give me your second parameter"<<endl;
+cout<<"Give me the second number"<<endl;
 cin>>second;
 
-int great = gcd(first, second);
+int great = GCD(first, second);
 
-cout<<"The greatest common divisor is "<<gcd<<endl;
+cout<<"The greatest common divisor is "<<great<<endl;
 
 return 0;
 }
